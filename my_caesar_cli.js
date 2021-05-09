57a0ca0c57555ace16a8320189494acca51b7b61
Ckiv <baltazar1995>
2021-05-09 13:02:25 +0300

    тестирование commander

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,1 +1,17 @@
-console.log(2+4);
+const commander = require('commander'); // include commander in git clone of commander repo
+const program = new commander.Command();
+
+program
+    .option('-i, --input <type>', 'input file')
+    .option('-o, --output <type>', 'output file')
+    .option('-s, --shift <type>', 'number shift')
+    .option('-a, --action <type>', 'encode/decode')
+
+
+program.parse(process.argv);
+
+const options = program.opts();
+if (options.input) console.log(options);
+if (options.output) console.log(options);
+if (options.shift) console.log(options);
+if (options.action) console.log(`- ${options.action}`);
